1ee53442944ebf90685cd8fe5f9324047e4e4f67
Roma <roma2002.rs@yandex.ru>
2024-06-03 23:23:19 +0700

    CONTE-20-move-config-vars-to-env

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,44 +1,39 @@
 package config
 
 import (
-	"github.com/ilyakaznacheev/cleanenv"
-	"os"
 	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
 )
 
 type Config struct {
-	GRPC       GRPC       `yaml:"grpc" env-required:"true"`
-	PostgreSQL PostgreSQL `yaml:"postgresql" env-required:"true"`
-	Migrations Migrations `yaml:"migrations" env-required:"true"`
+	GRPC       GRPC       `env-required:"true"`
+	PostgreSQL PostgreSQL `env-required:"true"`
+	Migrations Migrations `env-required:"true"`
 }
 
 type GRPC struct {
-	Port    uint16        `yaml:"port" env-default:"6970"`
-	Timeout time.Duration `yaml:"timeout" env-default:"5s"`
+	Port    uint16        `env:"GRPC_PORT" env-default:"6969"`
+	Timeout time.Duration `env:"GRPC_TIMEOUT" env-default:"5s"`
 }
 
 type PostgreSQL struct {
-	Host     string `yaml:"host" env-required:"true"`
-	Port     uint16 `yaml:"port" env-required:"true"`
-	User     string `yaml:"user" env-required:"true"`
-	Password string `yaml:"password" env-required:"true"`
-	DBName   string `yaml:"dbname" env-required:"true"`
-	SSLMode  string `yaml:"sslmode" env-required:"true" env-default:"disable"`
+	Host     string `env:"POSTGRES_HOST" env-required:"true"`
+	Port     uint16 `env:"POSTGRES_PORT" env-required:"true"`
+	User     string `env:"POSTGRES_USER" env-required:"true"`
+	Password string `env:"POSTGRES_PASSWORD" env-required:"true"`
+	DBName   string `env:"POSTGRES_DBNAME" env-required:"true"`
+	SSLMode  string `env:"POSTGRES_SSLMODE" env-required:"true" env-default:"disable"`
 }
 
 type Migrations struct {
-	Path string `yaml:"path" env-required:"true"`
+	Path string `env:"MIGRATIONS_PATH" env-required:"true"`
 }
 
-func MustLoadConfig(configPath string) *Config {
-	// check if file exists
-	if _, err := os.Stat(configPath); os.IsNotExist(err) {
-		panic("config file does not exists: " + configPath)
-	}
-
+func MustLoadConfig() *Config {
 	var cfg Config
 
-	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+	if err := cleanenv.ReadEnv(&cfg); err != nil {
 		panic("cannot read config: " + err.Error())
 	}
 
